d23d6224867fbc7194daa5a2719a22e749b11299
Przemek Badaruk <przemekkth@outlook.com>
2022-08-19 11:56:26 +0200

    restructure game class

--- src/game.h
+++ src/game.h
@@ -8,7 +8,20 @@ class Game
 {
 public:
     Game();
+
+    //Utilities
     constexpr static const QSize RESOLUTION = QSize(640, 480);
+    constexpr static const int FPS = 60;
+    enum class Direction{
+        NONE = 0, LEFT, RIGHT, DOWN
+    };
+    constexpr static const int GRID_SIZE = 32;
+    constexpr static const int GRID_WIDTH = RESOLUTION.width()/GRID_SIZE;
+    constexpr static const int GRID_HEIGHT = RESOLUTION.height()/GRID_SIZE;
+    static int convertPixelToGridPoint(int pixelVal);
+    static int convertGridPointToPixel(int gridPointVal);
+
+    //Sprite
     static const QString PATH_TO_SPACESHIP_PIXMAP;
     static const QString PATH_TO_PROJECTILE_PIXMAP;
     static const QString PATH_TO_ENEMY_PROJECTILE_PIXMAP;
@@ -19,12 +32,10 @@ public:
     static const QString PATH_TO_POWERUP_PIXMAP;
     static const QString PATH_TO_SPACESHIP_SPRITE_PIXMAP;
 
-    constexpr static const int FPS = 60;
-
     //Spaceship
     constexpr static const float SPACESHIP_SPEED = 4.f;
     constexpr static const float SPACESHIP_PROJECTILE_SPEED = 4.f;
-    constexpr static const int   SPACESHIP_SHOOT_COUNTER = 2 * (1000.0f/FPS);
+    constexpr static const int   SPACESHIP_SHOOT_COUNTER = 33;
     enum class SpaceshipStatus{
         NORMAL, SHIELD, TRIPLE_SHOOT, SUPER_SHOOT
     };
@@ -35,27 +46,16 @@ public:
     constexpr static const QPoint SPACESHIP_SUPER_PROJECTILE = QPoint(192, 0);
     static bool SPACESHIP_DEAD;
 
-
+    //Projectile
     enum class Projectile{
         SPACESHIP, ENEMY
     };
 
-    enum class Direction{
-        NONE = 0, LEFT, RIGHT, DOWN
-    };
-
+    //Enemy
     constexpr static const int ENEMY_SIZE = 32;
-
-    constexpr static const int GRID_SIZE = 32;
-    constexpr static const int GRID_WIDTH = RESOLUTION.width()/ENEMY_SIZE;
-    constexpr static const int GRID_HEIGHT = RESOLUTION.height()/ENEMY_SIZE;
     constexpr static float ENEMY_SPEED = 2;
-    static int convertPixelToGridPoint(int pixelVal);
-    static int convertGridPointToPixel(int gridPointVal);
-
     constexpr static const int COUNT_OF_ENEMY_ANIM_FRAMES = 2;
     constexpr static const int TIME_OF_ENEMY_FRAME = 250;
-
     static int   MIN_ENEMY_SHOOT_COUNTER;
     static int   MAX_ENEMY_SHOOT_COUNTER;
 
